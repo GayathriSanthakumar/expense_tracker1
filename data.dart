1e1e158406ec9085e275b55aca49ae1499fbe19c
Gayathri S <154979814+GayathriSanthakumar@users.noreply.github.com>
2025-03-14 14:03:59 +0530

    Create data.dart

---
+++ data.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/painting.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+var myData=[
+  {
+    'icon':const FaIcon(FontAwesomeIcons.burger),
+    'color':Color.fromARGB(255, 188, 182, 0),
+    'name':'Food',
+    'totalAmount': '-₹450.00',
+    'date':'Today',
+  },
+  {
+    'icon':const FaIcon(FontAwesomeIcons.bagShopping),
+     'color':Color.fromARGB(255, 67, 43, 174),
+    'name':'shopping',
+    'totalAmount': '-₹250.00',
+    'date':'Today',
+  }
+   {
+    'icon':const FaIcon(FontAwesomeIcons.heartCircleCheck),
+     'color':Color.fromRGBO(60, 174, 43, 1),
+    'name':'Health',
+    'totalAmount': '-₹250.00',
+    'date':'Yesterday',
+  }
+  {
+    'icon':const FaIcon(FontAwesomeIcons.plane),
+     'color':Color.fromRGBO(43, 63, 174, 1),
+    'name':'Travel',
+    'totalAmount': '-₹250.00',
+    'date':'Yesterday',
+  }
+];
